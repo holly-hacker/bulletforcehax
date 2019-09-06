93e1fa8fc53b6182410751d9673b0c7fa7524b51
HoLLy-HaCKeR <dario.db@hotmail.com>
2019-09-06 13:40:47 +0200

    Add cli argument to decode base64 commands

--- src/main.dart
+++ src/main.dart
@@ -1,8 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:bullet_force_hax/bullet_force_hax_cli.dart';
 
-Future main() async {
-  print('Hello, world!');
+Future main(List<String> arguments) async {
+  if (arguments.isNotEmpty && arguments[0] == "decodeb64") {
+    String input;
+    if (arguments.length == 2) {
+      input = arguments[1];
+    } else {
+      stdout.write("Enter base64 to decode: ");
+      input = stdin.readLineSync();
+    }
 
+    Uint8List inputBytes = base64.decode(input);
+
+    var parsed = ProtocolReader(inputBytes).readPacket();
+    print(parsed);
+  } else {
+    await doBot();
+  }
+}
+
+Future doBot() async {
   print('initializing');
   var bot = Bot();
 
