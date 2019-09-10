efcbc7bc0e047c718c18d4f808260dffc21ff930
HoLLy-HaCKeR <dario.db@hotmail.com>
2019-09-10 22:37:49 +0200

    Add GameProperties + tests

---
+++ lib/src/typed_wrappers/game_properties.dart
@@ -0,0 +1,72 @@
+import 'package:bullet_force_hax/bullet_force_hax.dart';
+import 'package:bullet_force_hax/src/typed_wrappers/basic_game_info.dart';
+import 'package:collection/collection.dart';
+
+class GameProperties extends BasicGameInfo {
+  String bannedWeaponMessage;
+  int gunGamePreset;
+  int hostId; // field 248
+  bool field249;
+  List<String> field250;
+  bool field254;
+  double matchCountdownTime;
+  bool matchStarted;
+  int maxPing;
+  bool roundStarted;
+  int scoreLimit;
+  double timeScale;
+
+  GameProperties();
+
+  GameProperties.fromMap(Map<Object, Object> map) : super.fromMap(map) {
+    bannedWeaponMessage = map['bannedweaponmessage'];
+    gunGamePreset = (map['gunGamePreset'] as SizedInt).value;
+    hostId = (map[SizedInt.byte(248)] as SizedInt).value;
+    field249 = map[SizedInt.byte(249)];
+    field250 = (map[SizedInt.byte(250)] as ProtocolArray).data.cast<String>();
+    field254 = map[SizedInt.byte(254)];
+    matchCountdownTime = (map['matchCountdownTime'] as SizedFloat).value;
+    matchStarted = map['matchStarted'];
+    maxPing = (map['maxPing'] as SizedInt).value;
+    roundStarted = map['roundStarted'];
+    scoreLimit = (map['scorelimit'] as SizedInt).value;
+    timeScale = (map['timeScale'] as SizedFloat).value;
+  }
+
+  Map<Object, Object> toMap() {
+    var map = super.toMap();
+    map['bannedweaponmessage'] = bannedWeaponMessage;
+    map['gunGamePreset'] = SizedInt.int(gunGamePreset);
+    map[SizedInt.byte(248)] = SizedInt.int(hostId);
+    map[SizedInt.byte(249)] = field249;
+    map[SizedInt.byte(250)] = ProtocolArray(DataType.String, field250);
+    map[SizedInt.byte(254)] = field254;
+    map['matchCountdownTime'] = SizedFloat.float(matchCountdownTime);
+    map['matchStarted'] = matchStarted;
+    map['maxPing'] = SizedInt.short(maxPing);
+    map['roundStarted'] = roundStarted;
+    map['scorelimit'] = SizedInt.int(scoreLimit);
+    map['timeScale'] = SizedFloat.float(timeScale);
+    return map;
+  }
+
+  bool operator ==(other) => equals(other);
+
+  bool equals(other) {
+    return identical(this, other)
+        || other is GameProperties
+        && super.equals(other)
+        && other.bannedWeaponMessage == bannedWeaponMessage
+        && other.gunGamePreset == gunGamePreset
+        && other.hostId == hostId
+        && other.field249 == field249
+        && DeepCollectionEquality().equals(other.field250, field250)
+        && other.field254 == field254
+        && other.matchCountdownTime == matchCountdownTime
+        && other.matchStarted == matchStarted
+        && other.maxPing == maxPing
+        && other.roundStarted == roundStarted
+        && other.scoreLimit == scoreLimit
+        && other.timeScale == timeScale;
+  }
+}
--- test/packet_reader_test.dart
+++ test/packet_reader_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:bullet_force_hax/bullet_force_hax.dart';
+import 'package:bullet_force_hax/src/typed_wrappers/game_properties.dart';
 import 'package:bullet_force_hax/src/typed_wrappers/listed_game_info.dart';
 import 'package:bullet_force_hax/src/typed_wrappers/player_properties.dart';
 import 'package:test/test.dart';
@@ -41,8 +42,32 @@ void main() {
         expect(packet.params[ParameterCode.ActorList].toString(), ProtocolArray(DataType.Integer, [SizedInt.int(2), SizedInt.int(3)]).toString());
 
         var gameProps = packet.params[ParameterCode.GameProperties] as Map;
-        expect(gameProps.length, 23);
-        // TODO: GameProperties
+        var gameProps1 = GameProperties.fromMap(gameProps);
+        expect(gameProps1, GameProperties()
+            ..matchCountdownTime = 0
+            ..field249 = true
+            ..maxPlayerCount = 10
+            ..field254 = true
+            ..field253 = true
+            ..eventCode = 0
+            ..field250 = ['roomName', 'mapName', 'modeName', 'password', 'dedicated', 'switchingmap', 'allowedweapons', 'eventcode', 'averagerank']
+            ..timeScale = 1
+            ..hostId = 2
+            ..password = ''
+            ..modeName = 'Conquest'
+            ..averageRank = 16
+            ..roundStarted = false
+            ..roomName = 'DefaultMatch'
+            ..mapName = 'Urban'
+            ..maxPing = 700
+            ..bannedWeaponMessage = 'Weapon not allowed! \n Check the pause menu for allowed weapons.'
+            ..dedicated = false
+            ..matchStarted = true
+            ..gunGamePreset = 0
+            ..allowedWeapons = [-1704068, 26371]
+            ..scoreLimit = 350
+            ..switchingMap = false
+        );
 
         var playerProps = packet.params[ParameterCode.PlayerProperties] as Map;
         var firstPlayerProps = playerProps[SizedInt.int(2)];
@@ -84,7 +109,6 @@ void main() {
       var testGame = ListedGameInfo()
       ..roomName = "TestRoom"
       ..password = "TestPassword"
-      ..password = "TestPassword"
       ..modeName = "aaa"
       ..mapName = "www"
       ..playerCount = 15
@@ -100,6 +124,41 @@ void main() {
       var bytes = (ProtocolWriter()..writeValue(map)).toBytes();
       var newMap = ProtocolReader(bytes).readValue();
       var newProps = ListedGameInfo.fromMap(newMap as Map);
+      expect(testGame, newProps);
+    });
+
+    test('$GameProperties', () {
+      var testGame = GameProperties()
+        // BasicGameInfo
+        ..roomName = "TestRoom"
+        ..password = "TestPassword"
+        ..modeName = "aaa"
+        ..mapName = "www"
+        ..maxPlayerCount = 16
+        ..averageRank = 42
+        ..allowedWeapons = [0x01234567, -0x01234567]
+        ..switchingMap = false
+        ..dedicated = true
+        ..eventCode = 0
+        ..field253 = false
+        // GameProperties
+        ..bannedWeaponMessage = "Banned weapon message"
+        ..gunGamePreset = 2
+        ..hostId = 248
+        ..field249 = true
+        ..field250 = ['roomName', 'mapName', 'modeName', 'password', 'dedicated', 'switchingmap', 'allowedweapons', 'eventcode', 'averagerank']
+        ..field254 = true
+        ..matchCountdownTime = 0
+        ..matchStarted = true
+        ..maxPing = 700
+        ..roundStarted = false
+        ..scoreLimit = 150
+        ..timeScale = 1;
+
+      var map = testGame.toMap();
+      var bytes = (ProtocolWriter()..writeValue(map)).toBytes();
+      var newMap = ProtocolReader(bytes).readValue();
+      var newProps = GameProperties.fromMap(newMap as Map);
       expect(testGame, newProps);
     });
   });
