fa3eef378d2f283e8abbc56bf8cfcac1fc0d12f8
HoLLy <dario.db@hotmail.com>
2019-12-25 02:47:08 +0100

    Enable logging and panic stacktraces

--- bulletforcehax/src/lib.rs
+++ bulletforcehax/src/lib.rs
@@ -1,10 +1,24 @@
 extern crate wasm_bindgen;
 
+use log::{Level, info};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+}
+
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // make panics better
+    console_error_panic_hook::set_once();
+
+    // allow logging to console
+    // could turn off logging in release builds for perf+size improvement, see https://crates.io/crates/console_log or https://docs.rs/log/0.4.10/log/#compile-time-filters
+    console_log::init_with_level(Level::Debug).unwrap();
+
+    info!("BulletForceHax wasm initialized!");
+    Ok(())
 }
 
 #[wasm_bindgen]
